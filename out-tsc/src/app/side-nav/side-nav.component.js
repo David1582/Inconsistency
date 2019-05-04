953d86d4535b8ced1ce478f5487ea6f0cdf0de75
Mr Bulborb <choonghumyon@gmail.com>
2019-05-04 15:23:30 -0700

    initial commit

---
+++ out-tsc/src/app/side-nav/side-nav.component.js
@@ -0,0 +1,19 @@
+import * as tslib_1 from "tslib";
+import { Component } from '@angular/core';
+var SideNavComponent = /** @class */ (function () {
+    function SideNavComponent() {
+    }
+    SideNavComponent.prototype.ngOnInit = function () {
+    };
+    SideNavComponent = tslib_1.__decorate([
+        Component({
+            selector: 'app-side-nav',
+            templateUrl: './side-nav.component.html',
+            styleUrls: ['./side-nav.component.css']
+        }),
+        tslib_1.__metadata("design:paramtypes", [])
+    ], SideNavComponent);
+    return SideNavComponent;
+}());
+export { SideNavComponent };
+//# sourceMappingURL=side-nav.component.js.map
